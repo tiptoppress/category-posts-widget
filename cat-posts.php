a89a9a60ab5624f7a96201722b60cd1902d01eba
Mrinal Kanti Roy <mkrdip@yahoo.com>
2015-07-07 05:31:59 +0600

    Updated Author & Plugin URI

--- cat-posts.php
+++ cat-posts.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Category Posts Widget
-Plugin URI: https://wordpress.org/plugins/category-posts/
+Plugin URI: http://mkrdip.me/category-posts-widget
 Description: Adds a widget that shows the most recent posts from a single category.
 Author: Mrinal Kanti Roy
 Version: 4.0
-Author URI: http://profiles.wordpress.org/mkrdip/
+Author URI: http://mkrdip.me
 */
 
 // Don't call the file directly
