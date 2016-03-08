d09227724621c00b72f1c5a44a77423cac5b231d
Mark S <mark@marksw.com>
2016-03-08 11:15:22 +0200

    move cropping utility into main file

--- cat-posts.php
+++ cat-posts.php
@@ -15,8 +15,6 @@ define( 'CAT_POST_PLUGINURL', plugins_url(basename( dirname(__FILE__))) . "/");
 define( 'CAT_POST_PLUGINPATH', dirname(__FILE__) . "/");
 define( 'CAT_POST_VERSION', "4.1.5");
 
-require_once( CAT_POST_PLUGINPATH . 'php/css-cropping.php' );
-
 /*
  * Iterate over all the widgets active at the page and call the callback for them
  * 
@@ -195,6 +193,41 @@ function category_posts_admin_scripts($hook) {
 		wp_register_script( 'category-posts-admin-js', CAT_POST_PLUGINURL . 'js/admin/category-posts-widget.js',array('jquery'),CAT_POST_VERSION,true );
 		wp_enqueue_script( 'category-posts-admin-js' );
 	}
+}
+
+/**
+ * Get image size
+ *
+ * $thumb_w, $thumb_h - the width and height of the thumbnail in the widget settings
+ * $image_w,$image_h - the width and height of the actual image being displayed
+ *
+ * return: an array with the width and height of the element containing the image
+ */
+function category_posts_get_image_size( $thumb_w,$thumb_h,$image_w,$image_h) {
+	
+	$image_size = array('image_h' => $thumb_h, 'image_w' => $thumb_w, 'marginAttr' => '', 'marginVal' => '');
+	$relation_thumbnail = $thumb_w / $thumb_h;
+	$relation_cropped = $image_w / $image_h;
+	
+	if ($relation_thumbnail < $relation_cropped) {
+		// crop left and right site
+		// thumbnail width/height ration is smaller, need to inflate the height of the image to thumb height
+		// and adjust width to keep aspect ration of image
+		$image_size['image_h'] = $thumb_h;
+		$image_size['image_w'] = $thumb_h / $image_h * $image_w; 
+		$image_size['marginAttr'] = 'margin-left';
+		$image_size['marginVal'] = ($image_size['image_w'] - $thumb_w) / 2;
+	} else {
+		// crop top and bottom
+		// thumbnail width/height ration is bigger, need to inflate the width of the image to thumb width
+		// and adjust height to keep aspect ration of image
+		$image_size['image_w'] = $thumb_w;
+		$image_size['image_h'] = $thumb_w / $image_w * $image_h; 
+		$image_size['marginAttr'] = 'margin-top';
+		$image_size['marginVal'] = ($image_size['image_h'] - $thumb_h) / 2;
+	}
+	
+	return $image_size;
 }
 
 /**
