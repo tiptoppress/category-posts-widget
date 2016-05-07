c4684e1f21aa3e2d24c1e02e6f4c11a6b2940807
Mark S <mark@marksw.com>
2016-05-07 20:15:54 +0300

    test front end behaviour on empty settings

--- tests/test-main.php
+++ tests/test-main.php
@@ -1,5 +1,21 @@
 <?php
 
-class testAll extends WP_UnitTestCase {
+define('NS','categoryPosts');
+
+class testWidgetFront extends WP_UnitTestCase {
+
+    /**
+     *  Check that there are no errors when instance is new
+     */
+    function testNoSetting() {
+        $className = NS.'\Widget';
+        $widget = new $className();
+        $widget->widget(array('before_widget'=>'',
+                              'after_widget'=>'',
+                              'before_title'=>'',
+                              'after_title'=>'',
+                              ),
+                        array());
+    }
 		
 }
