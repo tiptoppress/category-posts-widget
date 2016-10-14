1ffe10415716a2a008bd90ddc27c8eaa7708f400
Daniel Flöter <d.floe@gmx.de>
2016-10-14 20:19:27 +0200

    Make the code working with custom CSS Try to get rid of some variables but in consideration to perf. opt. I recommend keeping all variables

--- cat-posts.php
+++ cat-posts.php
@@ -1279,36 +1279,41 @@ function footer_script($number,$widgetsettings) {
 	<script type="text/javascript">
 		jQuery( document ).ready(function () {
 <?php
-			// change image dimensions
-			// calculate new image dimensions:
-			// if the layout-width have not enough space to show the regular source-width
+			// change cropped image dimensions
+			// @description: Calculate new image dimensions, if the layout-width have not enough space to show the regular source-width
 			// @since 4.7 
-?>
-			// namespace 			
+
+			// namespace 
+?>		
 			var cwp_namespace = cwp_namespace || {};
 			var cwp_namespace = {
+
 				Posts : {},
 				widget : null,
 
-				// constructor		
-				WidgetPosts : function Posts(widget) {	
-					<?php // variables ?>
-					this.firstListItem = widget.find('li:first');	<?php // First: use first for calculation ?>
-					this.firstThumb = this.firstListItem.find('.cat-post-thumbnail > span');	<?php // Thumb: with span (for image cropping) ?>
-					<?php // perf. opt.: declare this variables once and outside the callback ?>
-					this.maxThumbWidth = this.firstThumb.width();
-					this.ratio = this.firstThumb.width() / this.firstThumb.height();
-					this.ratioHeight = this.firstThumb.height() / this.firstThumb.find('img').height();	<?php // ratioHeight: between cropped- and source-image height ?>
-					this.allThumbs = widget.find('li .cat-post-thumbnail > span');		<?php // All: change all queried images ?>
-					this.allImgs = widget.find('li .cat-post-thumbnail > span > img');		<?php // Img: source-image ?>
+<?php			/* constructor */	?>	
+				WidgetPosts : function Posts(widget) {
 
-					<?php // functions ?>
+<?php 				/* variables */ ?>
+					this.firstListItem = widget.find('li:first');
+					this.firstThumb = this.firstListItem.find('.cat-post-thumbnail > span');
+					this.maxThumbSourceWidth = this.firstThumb.width();
+					this.ratio = this.firstThumb.width() / this.firstThumb.height();
+					this.ratioHeight = this.firstThumb.height() / this.firstThumb.find('img').height(); <?php /* ratioHeight: between cropped- and source-image height */ echo "\r\n" ?>
+					this.allThumbs = widget.find('.cat-post-thumbnail > span');
+					this.allImages = widget.find('.cat-post-thumbnail > span > img');		<?php /* Image: source-image */ echo "\r\n" ?>
+
+<?php 				/* functions */ ?>
 					this.changeImageSize = function changeImageSize() {
-						if(this.firstListItem.width() < this.firstThumb.width()-10 ||	<?php // if the layout-width have not enough space to show the regular source-width ?>
-							this.firstThumb.width() < this.maxThumbWidth) {				<?php // defined start and stop working width for the image: accomplish only the image width will be get smaller as the source-width ?>
-								this.allThumbs.width(this.firstListItem.width());
-								this.allThumbs.height(this.firstListItem.width()/this.ratio);
-								this.allImgs.height(this.firstListItem.width()/this.ratio/this.ratioHeight);
+
+						this.listItemLayoutWidth = this.firstListItem.width();
+						this.ImageLayoutWidth = this.firstThumb.outerWidth(true);
+
+						if(this.listItemLayoutWidth < this.ImageLayoutWidth ||	<?php /* if the layout-width have not enough space to show the regular source-width */ echo "\r\n" ?>
+							this.ImageLayoutWidth < this.maxThumbSourceWidth) {				<?php /* defined start and stop working width for the image: Accomplish only the image width will be get smaller as the source-width */ echo "\r\n" ?>
+								this.allThumbs.width(this.listItemLayoutWidth);
+								this.allThumbs.height(this.listItemLayoutWidth/this.ratio);
+								this.allImages.height(this.listItemLayoutWidth/this.ratio/this.ratioHeight);
 						}				
 					}
 				},
@@ -1317,7 +1322,7 @@ function footer_script($number,$widgetsettings) {
 			cwp_namespace.widget = jQuery('#category-posts-<?php echo $number?>');
 			cwp_namespace.Posts['<?php echo $number?>'] = new cwp_namespace.WidgetPosts(cwp_namespace.widget);
 
-			<?php // do only once on page load or on resize browser window ?>
+<?php 		/* do on page load or on resize the browser window */ echo "\r\n" ?>
 			jQuery(window).on('load resize', function() {
 				for (var post in cwp_namespace.Posts) {
 					cwp_namespace.Posts[post].changeImageSize();
