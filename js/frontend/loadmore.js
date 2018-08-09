359f518515a7380b74000ee1b1f39d5d02ee1d4d
mark kaplun <mark@marksw.com>
2018-08-09 17:48:32 +0300

    Fix file comment

--- js/frontend/loadmore.js
+++ js/frontend/loadmore.js
@@ -2,7 +2,7 @@
  * Category Posts Widget
  * https://github.com/tiptoppress/category-posts-widget
  *
- * Adds a widget that shows the most recent posts from a single category.
+ * JS for the "load more" functionality.
  *
  * Released under the GPLv2 license or later -  http://www.gnu.org/licenses/gpl-2.0.html
  */
