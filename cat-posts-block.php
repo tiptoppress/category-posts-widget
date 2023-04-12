d832bbe277e25b933f11dde22e3819e461b08286
Daniel Flöter <d.floe@gmx.de>
2023-04-12 18:24:52 +0200

    Delete old code

--- cat-posts-block.php
+++ cat-posts-block.php
@@ -23,98 +23,13 @@ function render_category_posts_block( $attributes ) {
 
 	$attr = $attributes;
 
-	$show_post_count = ! empty( $attributes['showPostCounts'] );
-
-	$class = '';
-
-	if ( ! empty( $attributes['displayAsDropdown'] ) ) {
-
-		$class .= ' category-posts-block-dropdown';
-
-		$dropdown_id = esc_attr( uniqid( 'category-posts-block-' ) );
-		$title       = __( 'Archives' );
-
-		/** This filter is documented in wp-includes/widgets/class-wp-widget-archives.php */
-		$dropdown_args = apply_filters(
-			'widget_archives_dropdown_args',
-			array(
-				'type'            => $attributes['groupBy'],
-				'format'          => 'option',
-				'show_post_count' => $show_post_count,
-				'order'           => $attributes['order'],
-			)
-		);
-
-		$dropdown_args['echo'] = 0;
-
-		$archives = wp_get_archives( $dropdown_args );
-
-		switch ( $dropdown_args['type'] ) {
-			case 'yearly':
-				$label = __( 'Select Year' );
-				break;
-			case 'monthly':
-				$label = __( 'Select Month' );
-				break;
-			case 'daily':
-				$label = __( 'Select Day' );
-				break;
-			case 'weekly':
-				$label = __( 'Select Week' );
-				break;
-			default:
-				$label = __( 'Select Post' );
-				break;
-		}
-
-		$label = esc_html( $label );
-
-		$block_content = '<label class="screen-reader-text" for="' . $dropdown_id . '">' . $title . '</label>
-	<select id="' . $dropdown_id . '" name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
-	<option value="">' . $label . '</option>' . $archives . '</select>';
-
-		return sprintf(
-			'<div class="%1$s">%2$s</div>',
-			esc_attr( $class ),
-			$block_content
-		);
-	}
-
-	$class .= ' category-posts-block-list';
-
-	/** This filter is documented in wp-includes/widgets/class-wp-widget-archives.php */
-	$archives_args = apply_filters(
-		'widget_archives_args',
-		array(
-			'type'            => $attributes['groupBy'],
-			'format'          => 'custom',
-			'show_post_count' => $show_post_count,
-			'order'           => $attributes['order'],
-		)
-	);
-
-	$archives_args['echo'] = 0;
-	
-	$archives = wp_get_archives( $archives_args );
-
-	$classnames = esc_attr( $class );
-
-	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classnames ) );
-
-	if ( empty( $archives ) ) {
-		return sprintf(
-			'<div %1$s>%2$s</div>',
-			$wrapper_attributes,
-			__( 'No archives to show.' )
-		);
-	}
-
 	// Get HTML
 	$widget = new Widget();
 	$instance = array();
 
 	$instance['asc_sort_order']      = $attributes['order'] === 'desc' ? false : true;
 	$instance['title']               = $attributes['title'];
+	$instance['hide_title']          = $attributes['hideTitle'];
 
 	$instance['footer_link_text']    = $attributes['footerLinkText'];
 	$instance['footer_link']         = $attributes['footerLink'];
